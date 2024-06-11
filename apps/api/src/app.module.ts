b42da1f1ca14015918a40bb7c5d46b694338b5c9
vitor <pereiravitor1218@gmail.com>
2024-06-11 19:46:56 -0300

    chore(api): Update app.module.ts to use configuration file for environment variables

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import configuration from "config/configuration";
 import { AuthModule } from "./auth/auth.module";
 import { ProductModule } from "./product/product.module";
 import { UserModule } from "./user/user.module";
 
 @Module({
-  imports: [ProductModule, UserModule, AuthModule, ConfigModule.forRoot()],
+  imports: [
+    ProductModule,
+    UserModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ apps/api/src/config/configuration.ts
@@ -0,0 +1,5 @@
+export default () => ({
+  port: parseInt(process.env.PORT, 10) || 3000,
+  databaseUrl: process.env.DATABASE_URL,
+  jwtSecret: process.env.JWT_SECRET ?? "secret",
+});
